0163903e51a51d0f96bdf5132fd1cb7e39c45bf8
Ricardo Brazileiro <rbrazileiro@gmail.com>
2017-07-03 22:33:41 -0300

    django app

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import settings
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.Index.as_view(), name="index"),
+    url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^product/$', views.ProductView.as_view(), name='about'),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/views.py
@@ -0,0 +1,25 @@
+# _*_ coding: utf-8 _*_
+from django.shortcuts import render, redirect, HttpResponseRedirect, HttpResponse
+from django.template.loader import get_template
+from django.contrib import messages
+from django.views import generic
+
+
+# Create your views here.
+class Index(generic.TemplateView):
+	template_name = 'core/index.html'
+        def get_context_data(self, **kwargs):
+            context = super(Index, self).get_context_data(**kwargs)
+            return context
+
+class AboutView(generic.TemplateView):
+	template_name = 'core/about.html'
+        def get_context_data(self, **kwargs):
+            context = super(AboutView, self).get_context_data(**kwargs)
+            return context
+
+class ProductView(generic.TemplateView):
+	template_name = 'core/product.html'
+        def get_context_data(self, **kwargs):
+            context = super(ProductView, self).get_context_data(**kwargs)
+            return context
